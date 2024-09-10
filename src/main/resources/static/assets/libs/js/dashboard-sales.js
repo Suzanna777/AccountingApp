3e8ef30699d8b8c9d86bd5c653a0755d7c32adb5
“Suzanna” <americandreamdoor@gmail.com>
2024-09-09 18:26:38 -0700

    Accounting App Application

--- src/main/resources/static/assets/libs/js/dashboard-sales.js
+++ src/main/resources/static/assets/libs/js/dashboard-sales.js
@@ -37,16 +37,6 @@ $(function() {
                             fontSize: 14,
                         }
                     },
-                    scales: {
-            yAxes: [{
-                ticks: {
-                    // Include a dollar sign in the ticks
-                    callback: function(value, index, values) {
-                        return '$' + value;
-                    }
-                }
-            }]
-        },
 
 
          scales: {
